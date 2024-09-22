2564117acec246fec9dd6f0579206f28d55ec4fe
Sheetal Verma <148845414+Sheetalv766@users.noreply.github.com>
2024-09-22 16:04:15 +0530

    Create AdventureDetailsPage.java

---
+++ app/src/test/java/pages/AdventureDetailsPage.java
@@ -0,0 +1,12 @@
+package qtriptest.pages;
+
+import com.beust.ah.A;
+import org.openqa.selenium.WebDriver;
+
+public class AdventureDetailsPage {
+    WebDriver driver;
+
+    public AdventureDetailsPage(WebDriver driver){
+        this.driver = driver;
+    }
+}
