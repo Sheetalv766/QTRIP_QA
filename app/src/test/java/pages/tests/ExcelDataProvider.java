37df37aa07ad6f9c8f20642f8f254f205f2de10c
Sheetal Verma <148845414+Sheetalv766@users.noreply.github.com>
2024-09-22 16:18:30 +0530

    Create ExcelDataProvider.java

---
+++ app/src/test/java/pages/tests/ExcelDataProvider.java
@@ -0,0 +1,13 @@
+package qtriptest.tests;
+
+import qtriptest.DP;
+import java.io.IOException;
+import org.testng.annotations.DataProvider;
+
+public class ExcelDataProvider {
+    
+    // @DataProvider(name = "userOnBoard")
+    // public Object[][] getUserOnBoardData() throws IOException{
+    //     return DP.readExcelFile();
+    // }
+}
